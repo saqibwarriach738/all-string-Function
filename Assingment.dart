ae0bc925d405a18f9cf7a5634ad03cef3c5680a9
Saqib Ejaz <73016154+saqibwarriach738@users.noreply.github.com>
2022-03-10 10:04:31 +0500

    Update Assingment.dart

--- Assingment.dart
+++ Assingment.dart
@@ -9,20 +9,36 @@ main(){
 
 
   print("My name is " + myName.toUpperCase());
+  // My name is SAQIB EJAZ
   print("My Cast is " + myCast.toLowerCase());
+  // My Cast is warriach
   print("My Country is " + myCountry);
+  // My Country is Pakistan
   print("I'm Belong From " + myCity.trim());
+  // I'm Belong From Mandi Bahudin
   print("My wight is "+ myWight.toString());
+  // My wight is 65
   print( currentAdd.length);
+  // 35
   print(myName.length);
+  // 10
   print(currentAdd.contains('Rawalpindi'));
+  // true
   print(currentAdd.replaceAll('Rawalpindi','Islamabad'));
+  // Hous no 13 Shally valley Islamabad
   print(myCountry.isEmpty);
+  // false
   print(myName.isNotEmpty);
+  // true
   print(myName.compareTo(myCast));
+  // -1
   print(myCity.trimLeft());
+  // Mandi Bahudin
   print(myCity.trimRight());
+  // Mandi Bahudin
   print(myCast.split(' 2 '));
+  // [Warriach]
+
 
 
 
